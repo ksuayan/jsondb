14d62f53db620f1bf9970df8f305460e7a2453b0
Kyo Suayan <ksuayan@gmail.com>
2013-02-17 14:35:41 -0800

    doh.  process.env.port

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ var view = require('./view');
 var api = require('./api');
 var trackdb = require('./itunes');
 var path = require('path');
-var PORT = 14808;
+var PORT = process.env.port || 8000;
 var app = express();
 
 console.log(conf);
