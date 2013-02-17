536d3fb98dd8fd30cddcccaac59a85e4359ffeca
Kyo Suayan <ksuayan@gmail.com>
2013-02-17 14:23:59 -0800

    bind to proper fqdn

--- server/conf.js
+++ server/conf.js
@@ -1,6 +1,6 @@
 module.exports = {
     app : {
-        host : 'http://localhost',
+        host : 'http://intense-beach-5801.herokuapp.com',
         name : 'JsonDB',
         author : 'Kyo Suayan',
         description : 'JsonDB is a sandbox application.',
