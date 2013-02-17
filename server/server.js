53277aaf202f06dcd54323e682c6fb5521301326
Kyo Suayan <ksuayan@gmail.com>
2013-02-17 14:33:30 -0800

    errr 14808

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ var view = require('./view');
 var api = require('./api');
 var trackdb = require('./itunes');
 var path = require('path');
-var PORT = 8000;
+var PORT = 14808;
 var app = express();
 
 console.log(conf);
