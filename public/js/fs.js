9b41b093d92c000cf56b20bd2933ae977abaa975
Kyo Suayan <ksuayan@gmail.com>
2013-06-06 14:57:35 -0700

    slideshow using setInterval

--- public/js/fs.js
+++ public/js/fs.js
@@ -1,4 +1,3 @@
-
 (function ($) {
     $.fn.fullscreen = function(options) {
 
@@ -7,24 +6,44 @@
             back: "#bg-back",
             bgHeightClass: 'bgheight',
             bgWidthClass: 'bgwidth',
-            images: ["image-001.png","image-002.png", "image-003.png"]
+            refreshInterval: 3000,
+            images: ["images/image-001.png","images/image-002.png","images/image-003.png"]
         }, options);
 
         var theWindow = $(window),
             $bg = $(settings.front),
-            aspectRatio = $bg.width() / $bg.height();
+            windowAspect = theWindow.width()/theWindow.height(),
+            imageAspect = $bg.width() / $bg.height(),
+            intervalHandler = null,
+            index = 0;
+
+        var refreshImage = function() {
+            if (index < settings.images.length - 1) {
+                index++;
+            } else {
+                index = 0;
+            }
+            $bg.attr("src", settings.images[index]);
+        };
+
+        var setRefreshInterval = function() {
+            if (!intervalHandler) {
+                intervalHandler = setInterval(refreshImage, settings.refreshInterval);
+            }
+        };
 
         function resizeBackgound() {
-            if ((theWindow.width() / theWindow.height()) < aspectRatio) {
+            windowAspect = theWindow.width()/theWindow.height();
+            if (windowAspect < imageAspect) {
                 $bg.removeClass().addClass(settings.bgHeightClass);
             } else {
                 $bg.removeClass().addClass(settings.bgWidthClass);
             }
         }
         theWindow.resize(resizeBackgound).trigger("resize");
+        setRefreshInterval();
         return this;
     };
-
 }(jQuery));
 
 
